8741b13ca6a6bca31be0302020ae817173354ddf
Mohammad hossein jadidi <jadidi.hosein@gmail.com>
2020-10-26 11:46:05 +0800

    initial commit

---
+++ MyBasicConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
